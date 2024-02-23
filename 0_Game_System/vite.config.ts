5365d5338b5c42fc8aac92fdd750943308d8d7fb
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-02-23 11:31:36 -0500

    HOTFIX: Update dependencies

--- 0_Game_System/vite.config.ts
+++ 0_Game_System/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
-import presetUno from '@unocss/preset-uno'
 
 
 // https://vitejs.dev/config/
